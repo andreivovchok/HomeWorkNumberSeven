31195a1f868310ce2dfe9724a348d57927a37efb
Andrei Vovchok <andreivovchok@bk.ru>
2021-08-10 20:10:40 +0300

    Добавил решение двух заданий

---
+++ HomeWorkNumberSeven/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWorkNumberSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент массива", i + 1);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            //Console.WriteLine();
+            foreach(int userNumber in numbers)
+            {
+                Console.Write($"{userNumber} ");
+            }
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            Console.WriteLine();
+            foreach (int sortedUserNumber in numbers)
+            {
+                Console.Write($"{sortedUserNumber} ");
+            }
+        }
+    }
+}
---
+++ TaskTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDimensional = { { 87, 25, 45 }, { 74, 12, 32 }, { 51, 97, 165 } };
+
+            for(int i = 0; i < 3; i++)
+            {
+                int maxValue = 0;
+                for(int n = 0; n < 3; n++)
+                {
+                    if(twoDimensional[i,n] > maxValue)
+                    {
+                        maxValue = twoDimensional[i, n];
+                    }
+                }
+                Console.WriteLine($"Максимальное число - {maxValue}");
+            }
+        }
+    }
+}
